6a9c1649c17de7f897a43cc5a831c49476e43904
alfajor <mcamilla20@alumnos.utalca.cl>
2023-10-20 00:35:56 -0300

    todavia sin terminar interior del modulo

--- client/src/components/PestañaModulo.jsx
+++ client/src/components/PestañaModulo.jsx
@@ -11,7 +11,12 @@ const PestañaModulo = () => {
   return (
     <>
     <h1>
-        Soy pestaña modulo
+        <div class="container">
+          <div class="botones           bg-red-500   ">  Botones </div>
+          <div class="filtros           bg-yellow-500        ">  Filtros </div>
+          <div class="Laterales          bg-teal-500       ">  Botones lateriales </div>
+          <div class="Interior-ramos     bg-indigo-500       ">  Interior ramos</div>
+        </div>
     </h1>
     </>
   )
