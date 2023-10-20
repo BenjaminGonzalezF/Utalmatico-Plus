9ebd1c2e5bb196acb3bd52c6429597e9cca2b5bf
BenjaminGonzalezF <begonzalez18@alumnos.utalca.cl>
2023-10-20 00:20:59 -0300

    Organización de carta de modulos

--- client/src/components/GeneracionCartaModulos.jsx
+++ client/src/components/GeneracionCartaModulos.jsx
@@ -7,12 +7,17 @@ function GeneracionCartaModulos() {
     { nombre: 'Modulo 1', descripcion: 'Este modulo va de 1', correo: 'correo1@example.com' },
     { nombre: 'Modulo 2', descripcion: 'Este modulo va de 2', correo: 'correo2@example.com' },
     { nombre: 'Modulo 3', descripcion: 'Este modulo va de 3', correo: 'correo3@example.com' },
+    { nombre: 'Modulo 1', descripcion: 'Este modulo va de 1', correo: 'correo1@example.com' },
+    { nombre: 'Modulo 2', descripcion: 'Este modulo va de 2', correo: 'correo2@example.com' },
+    { nombre: 'Modulo 3', descripcion: 'Este modulo va de 3', correo: 'correo3@example.com' },
+    { nombre: 'Modulo 1', descripcion: 'Este modulo va de 1', correo: 'correo1@example.com' },
+    { nombre: 'Modulo 2', descripcion: 'Este modulo va de 2', correo: 'correo2@example.com' },
+    { nombre: 'Modulo 3', descripcion: 'Este modulo va de 3', correo: 'correo3@example.com' },
   ];
 
   return (
-    <div className="App">
-      <section className="flex justify-center">
-        <div className='flex'>
+    <div className="flex">
+        <div className='flex flex-wrap justify-content"'> 
         {candidatos.map((candidato, index) => (
           <CartaModulo
             key={index}
@@ -22,7 +27,6 @@ function GeneracionCartaModulos() {
           />
         ))}
         </div>
-      </section>
       </div>
   );
 }
