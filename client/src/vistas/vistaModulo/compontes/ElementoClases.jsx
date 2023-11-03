b0c6fe32347d5e18bddb5e090e0a850006b276d0
alfajoruwu <mcamilla20@alumnos.utalca.cl>
2023-11-03 15:16:13 -0300

    cambio minusculo

--- client/src/vistas/vistaModulo/compontes/ElementoClases.jsx
+++ client/src/vistas/vistaModulo/compontes/ElementoClases.jsx
@@ -24,7 +24,7 @@ export default function ElementoClases({ nombreClase }) {
                     </div>
 
                     <div>
-                    <button onClick={irAEvaluacion}
+                    <button
                     class=" mt-3 ml-6 bg-orange-400 hover:bg-orange-300 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
                     <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
                     <span>Precentacion</span>
