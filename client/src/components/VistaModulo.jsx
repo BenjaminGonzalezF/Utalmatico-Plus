fd4bd0615010bcb7bc1b46983f26cf6103b2c6b7
BenjaminGonzalezF <begonzalez18@alumnos.utalca.cl>
2023-11-03 14:58:08 -0300

    Mostrar contenido de unidades

--- client/src/components/VistaModulo.jsx
+++ client/src/components/VistaModulo.jsx
@@ -1,4 +1,6 @@
 import RenderBotonesUnidades from '../vistas/vistaModulo/RenderBotonesUnidades';
+import RenderContenidoUnidades from '../vistas/vistaModulo/RenderContenidoUnidades';
+
 import BotonesModulos from './BotonesModulos';
 import BotonesProfe from './Pestaña_modulos/BotonesProfe';
 import BotonesAdmin from './Pestaña_modulosAdmin/BotonesAdmin';
@@ -32,7 +34,7 @@ export default function VistaModulo({nombre}) {
           <div class="botones     bg-yellow-500          "><BotonesModulos/> </div>
           <div class="filtros         bg-orange-500         "> <RenderBotonesUnidades/> </div>
           <div class="Laterales       bg-green-500         ">  Botones lateriales </div>
-          <div class="Interior-ramos     bg-teal-500      ">  Interior ramos</div>
+          <div class="Interior-ramos     bg-teal-500      "> <RenderContenidoUnidades/></div>
           
           
 
