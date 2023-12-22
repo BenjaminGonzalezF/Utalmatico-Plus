6531cb770a1dd03a6a45f0b798dfbdcc0af4802d
alfajoruwu <mcamilla20@alumnos.utalca.cl>
2023-12-21 22:16:13 -0300

    xd algo que no habia guardado

--- client/src/components/GeneracionCartaModulos.jsx
+++ client/src/components/GeneracionCartaModulos.jsx
@@ -44,17 +44,7 @@ function GeneracionCartaModulos() {
   const modulos = modulosget;
 
 
-  const candidatos = [
-    { nombre: 'Modulo 1', descripcion: 'Este modulo va de 1', correo: 'correo1@example.com' },
-    { nombre: 'Modulo 2', descripcion: 'Este modulo va de 2', correo: 'correo2@example.com' },
-    { nombre: 'Modulo 3', descripcion: 'Este modulo va de 3', correo: 'correo3@example.com' },
-    { nombre: 'Modulo 1', descripcion: 'Este modulo va de 1', correo: 'correo1@example.com' },
-    { nombre: 'Modulo 2', descripcion: 'Este modulo va de 2', correo: 'correo2@example.com' },
-    { nombre: 'Modulo 3', descripcion: 'Este modulo va de 3', correo: 'correo3@example.com' },
-    { nombre: 'Modulo 1', descripcion: 'Este modulo va de 1', correo: 'correo1@example.com' },
-    { nombre: 'Modulo 2', descripcion: 'Este modulo va de 2', correo: 'correo2@example.com' },
-    { nombre: 'Modulo 3', descripcion: 'Este modulo va de 3', correo: 'correo3@example.com' },
-  ];
+ 
 
   return (
     <div className="">
