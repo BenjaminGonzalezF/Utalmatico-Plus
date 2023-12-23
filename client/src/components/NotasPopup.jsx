62b713c8f3c414e5f5c54c0d7da9e2d0d1e55d3f
Antonia <afuenzalida20@alumnos.utalca.cl>
2023-12-22 22:38:08 -0300

    Arreglo de interfaz Notas

--- client/src/components/NotasPopup.jsx
+++ client/src/components/NotasPopup.jsx
@@ -3,6 +3,7 @@ import './CssNotas.css';
 
 const NotasPopup = ({ onClose }) => {
   const [mostrarTablaUnidad, setMostrarTablaUnidad] = useState(null);
+  const [mostrarTodasLasTablas, setMostrarTodasLasTablas] = useState(false);
 
   const buttonStyles = {
     fontSize: '20px',
@@ -20,11 +21,53 @@ const NotasPopup = ({ onClose }) => {
 
   const closeButtonStyles = {
     ...buttonStyles,
-    marginTop: '20px', 
+    marginTop: '20px',
   };
 
-  // Estado para mostrar u ocultar la información de todas las tablas
-  const [mostrarTodasLasTablas, setMostrarTodasLasTablas] = useState(false);
+  const notasUnidades = {
+    1: [
+      { nombre: 'Trabajo 1', unidad: 'U1', nota1: '' },
+      { nombre: 'Prueba 1', unidad: 'U1', nota1: '' },
+    ],
+    2: [
+      { nombre: 'Trabajo 1', unidad: 'U2', nota1: '' },
+      { nombre: 'Prueba 1', unidad: 'U2', nota1: '' },
+    ],
+    3: [
+      { nombre: 'Trabajo 1', unidad: 'U3', nota1: '' },
+      { nombre: 'Prueba 1', unidad: 'U3', nota1: '' },
+    ],
+  };
+
+  const renderTablaUnidad = (unidad) => (
+    <div key={unidad}>
+      <h3>Unidad {unidad}</h3>
+      <table className="notas-table">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Unidad</th>
+            <th>Nota 1</th>
+          </tr>
+        </thead>
+        <tbody>
+          {notasUnidades[unidad].map((nota) => (
+            <tr key={nota.nombre}>
+              <td>{nota.nombre}</td>
+              <td>{nota.unidad}</td>
+              <td>
+                <input
+                  type="text"
+                  className="nota-input"
+                  placeholder={`Ingrese nota 1 ${nota.nombre} ${nota.unidad}`}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 
   return (
     <div className="popup-overlay">
@@ -33,195 +76,31 @@ const NotasPopup = ({ onClose }) => {
         <div>
           <button
             onClick={() => {
-                setMostrarTodasLasTablas(!mostrarTodasLasTablas);
-                setMostrarTablaUnidad(null); 
-              }}
+              setMostrarTodasLasTablas(!mostrarTodasLasTablas);
+              setMostrarTablaUnidad(null);
+            }}
             className="unidad-button"
             style={buttonStyles}
           >
             Notas generales
           </button>
-          <button
-            onClick={() => {
-                setMostrarTodasLasTablas(false); 
-                setMostrarTablaUnidad(1); 
+          {[1, 2, 3].map((unidad) => (
+            <button
+              key={unidad}
+              onClick={() => {
+                setMostrarTodasLasTablas(false);
+                setMostrarTablaUnidad(unidad);
               }}
-            className="unidad-button"
-            style={buttonStyles}
-          >
-            Unidad 1
-          </button>
-          <button
-            onClick={() => {
-                setMostrarTodasLasTablas(false); 
-                setMostrarTablaUnidad(2);            
-              }}
-            className="unidad-button"
-            style={buttonStyles}
-          >
-            Unidad 2
-          </button>
-          <button
-            onClick={() => {
-                setMostrarTodasLasTablas(false); 
-                setMostrarTablaUnidad(3); 
-              }}
-            className="unidad-button"
-            style={buttonStyles}
-          >
-            Unidad 3
-          </button>
+              className="unidad-button"
+              style={buttonStyles}
+            >
+              Unidad {unidad}
+            </button>
+          ))}
         </div>
-        {mostrarTodasLasTablas ? (
-          <div>
-            <h3>Unidad 1</h3>
-            <table className="notas-table">
-              <thead>
-                <tr>
-                  <th>Unidad</th>
-                  <th>Nota 1</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Trabajo 1 U1</td>
-                  <td>
-                    <input type="text" className="nota-input" placeholder="Ingrese nota 1" />
-                  </td>
-                </tr>
-                {/* Agrega más filas según sea necesario */}
-              </tbody>
-            </table>
 
-            <h3>Unidad 2</h3>
-            <table className="notas-table">
-              <thead>
-                <tr>
-                  <th>Unidad</th>
-                  <th>Nota 1</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Trabajo 1 U2</td>
-                  <td>
-                    <input type="text" className="nota-input" placeholder="Ingrese nota 1" />
-                  </td>
-                </tr>
-                {/* Agrega más filas según sea necesario */}
-              </tbody>
-            </table>
-
-            <h3>Unidad 3</h3>
-            <table className="notas-table">
-              <thead>
-                <tr>
-                  <th>Unidad</th>
-                  <th>Nota 1</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Trabajo 1 U3</td>
-                  <td>
-  <input type="text" className="nota-input" placeholder="Ingrese nota 1" />
-</td>
-
-                </tr>
-                <tr>
-                  <td>Prueba 1 U3</td>
-                  <td>
-                    <input type="text" className="nota-input" placeholder="Ingrese nota 1" />
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        ) : null}
-
-        {mostrarTablaUnidad === 1 ? (
-          <div>
-            <h3>Unidad 1</h3>
-            <table className="notas-table">
-              <thead>
-                <tr>
-                  <th>Unidad</th>
-                  <th>Nota 1</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Trabajo 1 U1</td>
-                  <td>
-                    <input type="text" className="nota-input" placeholder="Ingrese nota 1" />
-                  </td>
-                </tr>
-                <tr>
-                  <td>Prueba 1 U3</td>
-                  <td>
-                    <input type="text" className="nota-input" placeholder="Ingrese nota 1" />
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        ) : null}
-
-        {mostrarTablaUnidad === 2 ? (
-          <div>
-            <h3>Unidad 2</h3>
-            <table className="notas-table">
-              <thead>
-                <tr>
-                  <th>Unidad</th>
-                  <th>Nota 1</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Trabajo 1 U2</td>
-                  <td>
-                    <input type="text" className="nota-input" placeholder="Ingrese nota 1" />
-                  </td>
-                </tr>
-                <tr>
-                  <td>Prueba 1 U3</td>
-                  <td>
-                    <input type="text" className="nota-input" placeholder="Ingrese nota 1" />
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        ) : null}
-
-        {mostrarTablaUnidad === 3 ? (
-          <div>
-            <h3>Unidad 3</h3>
-            <table className="notas-table">
-              <thead>
-                <tr>
-                  <th>Unidad</th>
-                  <th>Nota 1</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Trabajo 1 U3</td>
-                  <td>
-                    <input type="text" className="nota-input" placeholder="Ingrese nota 1" />
-                  </td>
-                </tr>
-                <tr>
-                  <td>Prueba 1 U3</td>
-                  <td>
-                    <input type="text" className="nota-input" placeholder="Ingrese nota 1" />
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        ) : null}
+        {mostrarTodasLasTablas && [1, 2, 3].map((unidad) => renderTablaUnidad(unidad))}
+        {mostrarTablaUnidad && renderTablaUnidad(mostrarTablaUnidad)}
 
         <button onClick={onClose} style={closeButtonStyles}>
           Cerrar
